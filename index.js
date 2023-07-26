952250f62778e59966e21cd5599392f317a9cd6a
Emmanuel Oppong <emmanueloppong@Emmanuels-MacBook-Pro-2.local>
2023-07-25 21:13:35 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,142 @@
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+const querystring = require("querystring");
+const axios = require("axios");
+
+const app = express();
+
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+const REDIRECT_URI = process.env.REDIRECT_URI;
+const FRONTEND_URI = process.env.FRONTEND_URI;
+const PORT = process.env.PORT || 8080;
+
+/*app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));*/
+// Priority serve any static files.
+app.use(express.static(path.resolve(__dirname, './client/build')));
+
+/*app.get("/", (req, res) => {
+  const data = {
+    name: "Mani",
+    isAwesome: true,
+  };
+  res.json(data);
+});*/
+
+generateRandomString = (length) => {
+  let text = "";
+  const possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (let i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+};
+
+const stateKey = "spotify_auth_state";
+
+app.get("/login", (req, res) => {
+  const state = generateRandomString(16);
+
+  res.cookie(stateKey, state);
+  const scope = [
+    'user-read-private',
+    'user-read-email',
+    'user-top-read',
+  ].join(' ');
+  const queryParams = querystring.stringify({
+    response_type: "code",
+    client_id: CLIENT_ID,
+    redirect_uri: REDIRECT_URI,
+    state: state,
+    scope: scope,
+  });
+  res.redirect(`https://accounts.spotify.com/authorize?${queryParams}`);
+});
+
+app.get("/callback", (req, res) => {
+  const code = req.query.code || null;
+  console.log(code)
+  axios({
+    method: "post",
+    url: "https://accounts.spotify.com/api/token",
+    data: querystring.stringify({
+      code: code,
+      redirect_uri: REDIRECT_URI,
+      grant_type: "authorization_code",
+    }),
+    headers: {
+      "content-type": "application/x-www-form-urlencoded",
+      'Authorization': 'Basic ' + (new Buffer.from(CLIENT_ID + ':' + CLIENT_SECRET).toString('base64'))
+    },
+  })
+    .then((response) => {
+      //console.log('response after callback')
+      /*if (response.status === 200) {
+        res.send(response.data)
+        res.redirect(`http://localhost:3000/`);
+      } else {
+        res.send(response)
+      }*/
+      if (response.status === 200) {
+        const { access_token, refresh_token, expires_in } = response.data;
+        const queryParams = querystring.stringify({
+          access_token,
+          refresh_token,
+          expires_in
+        });
+        res.redirect(`${FRONTEND_URI}?${queryParams}`);
+      } else {
+        res.redirect(
+          `/?${querystring.stringify({
+            error: "invalid_token",
+          })}`
+        );
+      }
+    })
+    .catch((error) => {
+      res.send(error);
+    });
+});
+
+app.get("/refresh_token", (req, res) => {
+  const { refresh_token } = req.query;
+  axios({
+    method: "post",
+    url: "https://accounts.spotify.com/api/token",
+    data: querystring.stringify({
+      grant_type: "refresh_token",
+      refresh_token: refresh_token,
+    }),
+    headers: {
+      "content-type": "application/x-www-form-urlencoded",
+      'Authorization': 'Basic ' + (new Buffer.from(CLIENT_ID + ':' + CLIENT_SECRET).toString('base64'))
+    },
+  })
+    .then((response) => {
+      console.log(`refresh token`);
+      if (response.status === 200) {
+        res.send(response.data);
+      }
+    })
+    .catch((error) => {
+      res.send(error);
+    });
+});
+
+// All remaining requests return the React app, so it can handle routing.
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './client/build', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Express app listening at http://localhost:${PORT}`);
+});
+
+module.exports = app;
